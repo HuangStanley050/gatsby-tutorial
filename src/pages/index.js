e618be9b385efa1bd7faa41c1a562f9aeebbc415
Stanley Huang <huang.stanley050@gmail.com>
2019-04-24 09:15:16 +1000

    added sass and bootstrap reactbootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import { Button } from "reactstrap";
 
 const IndexPage = () => (
   <Layout>
@@ -11,11 +12,12 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Button>hello</Button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
